ddb2ec1dcac1f8a8d9a5e81ea1ab455ee56159c0
vankokos <vano9995@gmail.com>
2021-01-26 12:56:37 +0200

    hotfix

--- js/script4.js
+++ js/script4.js
@@ -3,11 +3,9 @@ function myFunction () {
 
     if (elem.style.display == "grid") {
         elem.style.display = "none";
-        elem.style.height = "0px"
         document.querySelector('#seenbutton').innerHTML = "SHOW MORE PROJECTS"
     } else {
         elem.style.display = "grid";
-        elem.style.height = "400px"
         document.querySelector('#seenbutton').innerHTML = "SHOW LESS PROJECTS"
     }
 }
--- js/script5.js
+++ js/script5.js
@@ -1,4 +1,4 @@
-function toggleFunc () {
+function toggleFunc() {
     var elementsbg = document.querySelectorAll(".white")
     var elementsbgThree = document.querySelectorAll(".blacker")
     var text = document.querySelectorAll(".blacktext")
@@ -6,23 +6,24 @@ function toggleFunc () {
     var textTwo = document.querySelectorAll(".whiteorigin")
     var toggle = document.querySelector("#toggle")
 
-    for(let i = 0; i < elementsbg.length; i++) {
+    for (let i = 0; i < elementsbg.length; i++) {
         elementsbg[i].classList.toggle("black")
     }
-    for(let i = 0; i < text.length; i++) {
+    for (let i = 0; i < text.length; i++) {
         text[i].classList.toggle("whitetext")
     }
-    for(let i = 0; i< elementsbgTwo.length; i++) {
+    for (let i = 0; i < elementsbgTwo.length; i++) {
         elementsbgTwo[i].classList.toggle("blacktwo")
     }
-    for(let i = 0; i< elementsbgThree.length; i++) {
+    for (let i = 0; i < elementsbgThree.length; i++) {
         elementsbgThree[i].classList.toggle("blackest")
     }
-    for(let i = 0; i< textTwo.length; i++) {
-        textTwo[i].classList.toggle("blacktext")
+    for (let i = 0; i < textTwo.length; i++) {
+        textTwo[i].classList.toggle("blackorigin")
     }
 
-    if(toggle.innerHTML == "" || toggle.innerHTML == "NIGHT MODE") {
+
+    if (toggle.innerHTML == "" || toggle.innerHTML == "NIGHT MODE") {
         toggle.innerHTML = "LIGHT MODE"
     } else {
         toggle.innerHTML = "NIGHT MODE"
